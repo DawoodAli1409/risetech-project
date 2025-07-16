02e13c97dad4ebedad121d4c18f0276e97b01cd3
DawoodAli1409 <alidawood1409@gmail.com>
2025-07-16 14:24:26 +0500

    Initial commit with homepage for risetech-project

--- project/vite.config.js
+++ project/vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/risetech-project/',
   plugins: [react()],
-})
+  base: '/risetech-project/' 
+});
